87cd0a3d2dbff13d06ae748b44ae188e66b65d0b
chrlesdev <charlescornelio0813@gmail.com>
2026-01-19 18:42:19 +0700

    feat: creating card still hard coded

--- app/web/src/components/store/Dashboard.tsx
+++ app/web/src/components/store/Dashboard.tsx
@@ -1,62 +1,117 @@
 import ShopNavbar from "./NavbarShop";
-import { FaTshirt } from "react-icons/fa";
-import { GiSleevelessJacket } from "react-icons/gi";
-import { IoShirtSharp } from "react-icons/io5";
-import { GiTrousers } from "react-icons/gi";
-import { PiBagFill } from "react-icons/pi";
-import { GiRunningShoe } from "react-icons/gi";
-import { CgMenuGridO } from "react-icons/cg";
+// import { FaTshirt } from "react-icons/fa";
+// import { GiSleevelessJacket } from "react-icons/gi";
+// import { IoShirtSharp } from "react-icons/io5";
+// import { GiTrousers } from "react-icons/gi";
+// import { PiBagFill } from "react-icons/pi";
+// import { GiRunningShoe } from "react-icons/gi";
+// import { CgMenuGridO } from "react-icons/cg";
+import Image from "next/image";
+import { Card, CardFooter, CardTitle, CardContent, CardDescription } from "@/components/ui/card";
 
 export default function Dashboard() {
-  const categoriesCollection = [
-    {
-      id: 1,
-      icon: <FaTshirt />,
-    },
-    {
-      id: 2,
-      icon: <GiSleevelessJacket />,
-    },
-    {
-      id: 3,
-      icon: <IoShirtSharp />,
-    },
-    {
-      id: 4,
-      icon: <GiTrousers />,
-    },
-    {
-      id: 5,
-      icon: <PiBagFill />,
-    },
-    {
-      id: 6,
-      icon: <GiRunningShoe />,
-    },
-    {
-      id: 7,
-      icon: <CgMenuGridO />,
-    },
-  ];
+  //   const categoriesCollection = [
+  //     {
+  //       id: 1,
+  //       icon: <FaTshirt />,
+  //     },
+  //     {
+  //       id: 2,
+  //       icon: <GiSleevelessJacket />,
+  //     },
+  //     {
+  //       id: 3,
+  //       icon: <IoShirtSharp />,
+  //     },
+  //     {
+  //       id: 4,
+  //       icon: <GiTrousers />,
+  //     },
+  //     {
+  //       id: 5,
+  //       icon: <PiBagFill />,
+  //     },
+  //     {
+  //       id: 6,
+  //       icon: <GiRunningShoe />,
+  //     },
+  //     {
+  //       id: 7,
+  //       icon: <CgMenuGridO />,
+  //     },
+  //   ];
 
   return (
     <main className="w-screen">
-      <div>
+      <div className="flex justify-center items-center w-screen">
         <ShopNavbar />
       </div>
-      <div className="p-2 border-2 w-screen h-96 border-black">Banner</div>
-      <div className="p-6">
-        <ul className="flex justify-center items-center space-x-5">
-          {categoriesCollection.map((item) => (
-            <li
-              key={item.id}
-              id={item.id.toString()}
-              className="bg-gray-300 w-16 flex justify-center items-center rounded-full h-16 hover:-translate-y-3 transition cursor-pointer duration-300 ease-out hover:bg-gray-400 shadow-sm hover:shadow-md text-5xl"
-            >
-              {item.icon}
-            </li>
-          ))}
-        </ul>
+      <div className="  w-screen h-96 items-center justify-center flex">
+        <div className="p-2 border-2 w-[900px] h-80 border-black items-center justify-center flex rounded-2xl">
+          {/* <Image height={500} width={500} alt="Shop items images" src={"https://i.pinimg.com/1200x/f1/ec/27/f1ec27dc53600c6985571ba4b9f22891.jpg"} /> */}
+        </div>
+      </div>
+
+      <div className="w-screen h-96 flex justify-center items-center gap-3">
+        <Card className="h-[340px] w-[200px] flex justify-center items-center border-2 ">
+          <CardContent className="p-0 w-full flex justify-center">
+            <Image src="https://i.pinimg.com/736x/8c/db/e1/8cdbe123010c380e20f264a8fdd57938.jpg" alt="Shop items images" width={200} height={200} className="rounded-md h-[205px] w-[200px]" />
+          </CardContent>
+          <CardTitle className=" w-50 justify-center items-center flex">Headphone JBL/ AA-10</CardTitle>
+          <div className="flex gap-2">
+            <CardDescription>decription1</CardDescription>
+            <CardDescription>decription1</CardDescription>
+          </div>
+          <CardFooter>footer1</CardFooter>
+        </Card>
+
+        <Card className="h-[340px] w-[200px] flex justify-center items-center border-2 ">
+          <CardContent className="p-0 w-full flex justify-center">
+            <Image src="https://i.pinimg.com/736x/8c/db/e1/8cdbe123010c380e20f264a8fdd57938.jpg" alt="Shop items images" width={200} height={200} className="rounded-md h-[205px] w-[200px]" />
+          </CardContent>
+          <CardTitle className=" w-50 justify-center items-center flex">Headphone JBL/ AA-10</CardTitle>
+          <div className="flex gap-2">
+            <CardDescription>decription1</CardDescription>
+            <CardDescription>decription1</CardDescription>
+          </div>
+          <CardFooter>footer1</CardFooter>
+        </Card>
+
+        <Card className="h-[340px] w-[200px] flex justify-center items-center border-2 ">
+          <CardContent className="p-0 w-full flex justify-center">
+            <Image src="https://i.pinimg.com/736x/8c/db/e1/8cdbe123010c380e20f264a8fdd57938.jpg" alt="Shop items images" width={200} height={200} className="rounded-md h-[205px] w-[200px]" />
+          </CardContent>
+          <CardTitle className=" w-50 justify-center items-center flex">Headphone JBL/ AA-10</CardTitle>
+          <div className="flex gap-2">
+            <CardDescription>decription1</CardDescription>
+            <CardDescription>decription1</CardDescription>
+          </div>
+          <CardFooter>footer1</CardFooter>
+        </Card>
+
+        <Card className="h-[340px] w-[200px] flex justify-center items-center border-2 ">
+          <CardContent className="p-0 w-full flex justify-center">
+            <Image src="https://i.pinimg.com/736x/8c/db/e1/8cdbe123010c380e20f264a8fdd57938.jpg" alt="Shop items images" width={200} height={200} className="rounded-md h-[205px] w-[200px]" />
+          </CardContent>
+          <CardTitle className=" w-50 justify-center items-center flex">Headphone JBL/ AA-10</CardTitle>
+          <div className="flex gap-2">
+            <CardDescription>decription1</CardDescription>
+            <CardDescription>decription1</CardDescription>
+          </div>
+          <CardFooter>footer1</CardFooter>
+        </Card>
+
+        <Card className="h-[340px] w-[200px] flex justify-center items-center border-2 ">
+          <CardContent className="p-0 w-full flex justify-center">
+            <Image src="https://i.pinimg.com/736x/8c/db/e1/8cdbe123010c380e20f264a8fdd57938.jpg" alt="Shop items images" width={200} height={200} className="rounded-md h-[205px] w-[200px]" />
+          </CardContent>
+          <CardTitle className=" w-50 justify-center items-center flex">Headphone JBL/ AA-10</CardTitle>
+          <div className="flex gap-2">
+            <CardDescription>decription1</CardDescription>
+            <CardDescription>decription1</CardDescription>
+          </div>
+          <CardFooter>footer1</CardFooter>
+        </Card>
       </div>
     </main>
   );
